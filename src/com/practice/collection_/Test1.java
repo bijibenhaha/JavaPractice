3a4dbcd6cc6c1151fc93a5f36360734c16c96bb4
duqingyang <duqingyang@atosenet.com>
2024-01-29 19:03:04 +0000

    初始化git项目

---
+++ src/com/practice/collection_/Test1.java
@@ -0,0 +1,11 @@
+package com.practice.collection_;
+
+/**
+ * @author Qingyang Du
+ * @version 1.0
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello, I am Test1.java");
+    }
+}
